f39b39a4dde34839ad078a806c2e90d3f40b4ba2
swana <m@m>
2013-05-23 13:26:24 +0100

    Implemented scripts Install.ps1 and Uninstall.ps1 for being able to add custom content without referencing items in the project. This excludes them from source control automatically too.

---
+++ Content/tools/Uninstall.ps1
@@ -0,0 +1,40 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+function RemoveFromOutput([System.IO.DirectoryInfo]$currentDI, [System.IO.DirectoryInfo]$targetDI,[string] $relativeDirString)
+{
+	foreach($contentDirItem in $currentDI.GetDirectories())
+	{
+		Write-Output Enumerating subdirectory $contentDirItem.Name
+		RemoveFromOutput $contentDirItem $targetDI ($relativeDirString + $contentDirItem.Name + "\")
+	}
+	
+	foreach($contentDirFile in $currentDI.GetFiles())
+	{
+		$fullFileNameToCopyStr = $targetDI.FullName + $relativeDirString + $contentDirFile.Name
+		
+		$fullFileToCopyFI = New-Object System.IO.FileInfo $fullFileNameToCopyStr
+		if($fullFileToCopyFI.Exists)
+		{
+			Write-Output Deleting $fullFileToCopyFI.FullName
+			$fullFileToCopyFI.Delete()
+		}
+	}
+}
+
+$projectDir = (Get-Item $project.FullName).Directory
+
+$ProjectDI = $projectDir
+$InstallDI = (New-Object system.IO.DirectoryInfo $installPath)
+
+$projectPath = $projectDir.Fullname
+
+$installPathString = $InstallDI.FullName
+$projectPathString = $ProjectDI.FullName
+
+$ContentUnreferencedDI = [System.IO.DirectoryInfo]$InstallDI.GetDirectories("ContentUnreferenced")[0]
+
+if($ContentUnreferencedDI -ne $null)
+{
+	RemoveFromOutput $ContentUnreferencedDI $ProjectDI "\"
+}
+
